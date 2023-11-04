5d31d7e2df86085ee34e0dd0ea6508d3f1d99888
Arnish <arnishsatasiya333@gmail.com>
2023-11-04 23:48:51 +0530

    add Slidebar component

--- Frontend/src/Visualization.js
+++ Frontend/src/Visualization.js
@@ -1,8 +1,11 @@
 import React, { useLayoutEffect } from "react";
-import { initialize} from "./main";
+import { initialize, updateCrosshair} from "./main";
+import Sidebar from './Sidebar';
 
 // export default function Visualization() {
 const Visualization = ({ uploadedFile }) => {
+
+    const [sidebarVisible, setSidebarVisible] = useState(false);
 
     useLayoutEffect(() => {
 
@@ -18,10 +21,26 @@ const Visualization = ({ uploadedFile }) => {
     //     }
     // }
 
+    const handleEnter = (coordinates) => {
+        const { x, y, z } = coordinates;
+        updateCrosshair("sliceX", x, y, z);
+        updateCrosshair("sliceY", x, y, z);
+        updateCrosshair("sliceZ", x, y, z);
+      }
+
+      const toggleSidebar = () => {
+        setSidebarVisible(!sidebarVisible);
+      }
+
     return (
 
         <div className="container1">
             <h1>Visualization Page</h1>
+            <button onClick={toggleSidebar}>
+            {/* You can replace this with an actual icon */}
+            ☰
+            </button>
+            { sidebarVisible && <Sidebar onEnter={handleEnter} /> }
             {/* <div id="threeD" style={{ backgroundColor: '#000000', width: '100%', height: '100%', marginBottom: '2px' }}></div> */}
 
             <div id="threeD"></div>
